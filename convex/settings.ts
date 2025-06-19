b34e42bf763fb98e53ec6c104a38fe4ff66c4696
rlqd <83128595+rlqd@users.noreply.github.com>
2025-06-19 18:18:07 +0100

    Fix building

--- convex/settings.ts
+++ convex/settings.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { query, mutation, internalMutation } from "./_generated/server";
+import { query, mutation } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 
 export const addKey = mutation({
